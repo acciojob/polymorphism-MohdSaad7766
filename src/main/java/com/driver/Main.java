6a812e3917d08dcbed0bbb604bd2cd7e878c937f
Ansari Mohd Saad <ansarisaad777666@gmail.com>
2025-03-16 19:33:10 +0530

    Implementing the Polymorphism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,7 +1,7 @@
 package com.driver;
 
 public class Main {
-    static class Product{
+    public static class Product{
         public int product(int x,int y){
             return x * y;
         }
@@ -17,9 +17,9 @@ public class Main {
     public static void main(String[] args) {
         Product p = new Product();
 
-        p.product(2,3);
-        p.product(2,3,5);
-        p.product(2.3,3.5);
+        System.out.println(p.product(2,3));
+        System.out.println(p.product(2,3,5));
+        System.out.println(p.product(2.3,3.5));
     }
 }
 
