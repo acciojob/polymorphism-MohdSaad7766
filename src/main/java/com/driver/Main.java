fa0516897ec7905a6a8869397c8f37c08a08f95c
Ansari Mohd Saad <ansarisaad777666@gmail.com>
2025-03-16 19:19:20 +0530

    This class contains 3 methods with the same name but different parameter lists. Despite having the same name, the class compiles and runs successfully.

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,24 +1,24 @@
 package com.driver;
 
 public class Main {
+    static class Product{
+        public int product(int x,int y){
+            return x * y;
+        }
+
+        public int product(int x,int y,int z){
+            return x * y * z;
+        }
+
+        public double product(double x,double y){
+            return x * y;
+        }
+    }
     public static void main(String[] args) {
         Product p = new Product();
-        p.product(2,3);
-        p.product(2,3,4);
-        p.product(2.3,4.6);
+        System.out.println(p.product(2,3));
+        System.out.println(p.product(2,3,5));
+        System.out.println(p.product(2.3,3.5));
     }
 }
 
-class Product{
-    public int product(int x,int y){
-        return x * y;
-    }
-
-    public int product(int x,int y,int z){
-        return x * y * z;
-    }
-
-    public double product(double x,double y){
-        return x * y;
-    }
-}
